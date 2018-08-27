e0f47f3a9d36189c5527ee64d4702e5680bc2f78
BazzBubbles <42724688+BazzBubbles@users.noreply.github.com>
2018-08-26 23:29:59 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+{
+  "ClientId": 483412263684931594,
+  "Token": "NDgzNDEyMjYzNjg0OTMxNTk0.DmThlQ.G7poWR74951aS-Kf-4RYNoRw_ro",
+  "OwnerIds": [
+  206628633857490944,
+  423153125990596609  
+  ],
+  "LoLApiKey": "69df6e78bc9be1b8661d12a773a5dab7",
+  "GoogleApiKey": "AIzaSyAEgBIO_HK00LfgvEGHrW7h2MAITL6sM3w",
+  "MashapeKey": "S1LZHzjgkkmshZLX68U67H9vzpZlp1WsBRRjsnEENbddf5MMdx",
+  "OsuApiKey": "e776ded3234bc0d23f47b471744516feab6ffe2c",
+  "SoundCloudClientId": "",
+  "CleverbotApiKey": "d3zBVTwCDX3VVJrUgBRA3TIQi0ws6JFh",
+  "CarbonKey": "",
+  "Db": null,
+  "TotalShards": 1,
+  "PatreonAccessToken": "",
+  "PatreonCampaignId": "334038",
+  "RestartCommand": null,
+  "ShardRunCommand": "",
+  "ShardRunArguments": "",
+  "ShardRunPort": null,
+  "TwitchClientId": null,
+  "RedisOptions": null
+}
